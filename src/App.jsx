1bb6539f2aa1add6f8d37a835eb783f978516f3e
laporejero <laporejero1297@gmail.com>
2026-04-21 14:59:17 +0800

    added languages data and CSS update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Status from './components/Status.jsx'
+import { languages } from '../languages.js'
 import './App.css'
 
 function App() {
-  
+
+  const languageElements = languages.map((lang) => {
+    return (
+      <span 
+        className='language-chip' 
+        style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
+        key={lang.name}
+      >
+        {lang.name}
+      </span>
+    )
+  })
+
 
   return (
     <>
       <Header />
       <Status />
+      <div className='language-chips-container'>
+        {languageElements}
+      </div>
     </>
   )
 }
