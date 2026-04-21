4332b811d75cf54372ea04aa26c86ddc9ab6bf47
laporejero <laporejero1297@gmail.com>
2026-04-21 13:59:51 +0800

    Game status component initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
+import Status from './components/Status.jsx'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Status />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="header">
+        <header>
             <h1>Assembly: Endgame</h1>
             <p>Guess the word within 8 attemps to keep the programming world safe from Assembly!</p>
         </header>
