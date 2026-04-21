5e4c38c83a6042f550ce34602b979f211861c113
laporejero <laporejero1297@gmail.com>
2026-04-21 15:31:42 +0800

    added state and the Word section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ import { languages } from '../languages.js'
 import './App.css'
 
 function App() {
+  /**
+   * Goal: Build out the main parts of our app
+   * 
+   * Challenge: 
+   * 1. Save a "currentWord" in state. Initialize as "react".
+   * 2. Map over the letters of the word (you'll need to turn 
+   *    the string into an array of letters first) and display
+   *    each one as a <span>. Capitalize the letters when
+   *    displaying them.
+   * 3. Style to look like the design. You can get the underline 
+   *    effect on the box using `border-bottom`.
+   */
+  const [currentWord, setCurrentWord] = useState("react")
 
   const languageElements = languages.map((lang) => {
     return (
@@ -18,6 +31,9 @@ function App() {
     )
   })
 
+  const letterElements = currentWord.split("").map((letter, index) => (
+      <span key={index} className='box'>{letter.toUpperCase()}</span>
+  ))
 
   return (
     <>
@@ -26,6 +42,9 @@ function App() {
       <div className='language-chips-container'>
         {languageElements}
       </div>
+      <section className='word'>
+        {letterElements}
+      </section>
     </>
   )
 }
