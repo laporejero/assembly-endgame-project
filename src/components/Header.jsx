48426a45c81a6f4f39ceeb5bc122770429e85a07
laporejero <laporejero1297@gmail.com>
2026-04-21 13:36:13 +0800

    Initian commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word within 8 attemps to keep the programming world safe from Assembly!</p>
+        </header>
+    )
+}
